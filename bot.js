777f6158d39393a97e66b7abe01cb6e8d0af6810
supermike444 <hoellemj@mail.uc.edu>
2016-11-11 15:05:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,24 +4,32 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^ayy$/;
+  var request = JSON.parse(this.req.chunks[0]);
+  var ayyRegex = /^ayy$/;
+  var wewRegex = /^wew$/;
+  
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && ayyRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage("lmao");
     this.res.end();
-  } else {
+  }
+  else if (request.text && wewRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("lad");
+    this.res.end();
+  }
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-function postMessage() {
+function postMessage(input) {
   var botResponse, options, body, botReq;
 
-  botResponse = "lmao";
+  botResponse = "input";
 
   options = {
     hostname: 'api.groupme.com',
